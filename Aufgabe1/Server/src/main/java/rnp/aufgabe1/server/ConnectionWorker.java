1116bb5388ad2fdefb2b1b3981bd97ae2a373117
flbaue <flbaue@posteo.de>
2014-10-30 12:08:34 +0100

    remove \u0000 characters

--- Aufgabe1/Server/src/main/java/rnp/aufgabe1/server/ConnectionWorker.java
+++ Aufgabe1/Server/src/main/java/rnp/aufgabe1/server/ConnectionWorker.java
@@ -14,104 +14,108 @@ import java.net.Socket;
  */
 public class ConnectionWorker implements Runnable {
 
-	private static int instanceCounter = 0;
+    private static int instanceCounter = 0;
 
-	public final int instance;
-	private final Socket clientSocket;
-	private final CommandProcessor processor = new CommandProcessor(this);
-	private final Server server;
-	private boolean isStopped = false;
+    public final int instance;
+    private final Socket clientSocket;
+    private final CommandProcessor processor = new CommandProcessor(this);
+    private final Server server;
+    private boolean isStopped = false;
 
-	public ConnectionWorker(Socket clientSocket, Server server) {
-		this.clientSocket = clientSocket;
-		this.server = server;
-		this.instance = ++instanceCounter;
-	}
+    public ConnectionWorker(Socket clientSocket, Server server) {
+        this.clientSocket = clientSocket;
+        this.server = server;
+        this.instance = ++instanceCounter;
+    }
 
-	@Override
-	public void run() {
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					clientSocket.getInputStream(), "UTF-8"));
-			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
-					clientSocket.getOutputStream(), "UTF-8"));
+    @Override
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    clientSocket.getInputStream(), "UTF-8"));
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+                    clientSocket.getOutputStream(), "UTF-8"));
 
-			while (!isStopped) {
-				String input = readIncomingText(in, 255);
-				String output = processInput(input);
+            while (!isStopped) {
+                String input = readIncomingText(in, 255);
+                String output = processInput(input);
 
-				out.write(output);
-				out.flush();
-			}
+                out.write(output);
+                out.flush();
+            }
 
-			in.close();
-			out.close();
+            in.close();
+            out.close();
 
-		} catch (IOException e) {
-			// e.printStackTrace();
-		} finally {
-			closeSocket();
-		}
+        } catch (IOException e) {
+            // e.printStackTrace();
+        } finally {
+            closeSocket();
+        }
 
-		server.removeConnection(this);
-	}
+        server.removeConnection(this);
+    }
 
-	private String processInput(String input) {
-		String output = processor.process(input);
-		if (output.startsWith("BYE") || output.startsWith("OK_BYE")) {
-			isStopped = true;
-		}
-		return output;
-	}
+    private String processInput(String input) {
+        String output = processor.process(input);
+        if (output.startsWith("BYE") || output.startsWith("OK_BYE")) {
+            isStopped = true;
+        }
+        return output;
+    }
 
-	public boolean stopServer(String secretToken) {
-		return server.stop(secretToken);
-	}
+    public boolean stopServer(String secretToken) {
+        return server.stop(secretToken);
+    }
 
-	private String readIncomingText(BufferedReader in, int length)
-			throws IOException {
-		char[] buffer = new char[length];
-		int c = 0;
-		int count = 0;
-		while (c != 0x0A && count < 255) {
-			c = in.read();
-			if (c != -1) {
-				buffer[count] = (char) c;
-				count++;
-			}
-		}
+    private String readIncomingText(BufferedReader in, int length)
+            throws IOException {
+        char[] buffer = new char[length];
+        int c = 0;
+        int count = 0;
+        while (c != 0x0A && count < 255) {
+            c = in.read();
+            if (c != -1) {
+                buffer[count] = (char) c;
+                count++;
+            }
+        }
 
-		String input = String.valueOf(buffer);
+        String input = String.valueOf(buffer);
 
-		return input;
+        int end = input.indexOf('\u0000');
+        if (end != -0) {
+            input = input.substring(0, end);
+        }
+        return input;
 
-	}
+    }
 
-	public void closeSocket() {
-		try {
-			clientSocket.close();
-		} catch (IOException e) {
-			// throw new RuntimeException("Error while closing the socket", e);
-		}
-	}
+    public void closeSocket() {
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            // throw new RuntimeException("Error while closing the socket", e);
+        }
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
-		ConnectionWorker that = (ConnectionWorker) o;
+        ConnectionWorker that = (ConnectionWorker) o;
 
-		if (instance != that.instance)
-			return false;
+        if (instance != that.instance)
+            return false;
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public int hashCode() {
-		return instance;
-	}
+    @Override
+    public int hashCode() {
+        return instance;
+    }
 }
